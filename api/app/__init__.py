60444f83ca544790b257334b58e83da69431163f
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-12-06 17:28:39 +0200

    added init. When you start the server, all config, db, etc. will be get from this initilization file.

---
+++ api/app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../../client/templates')) #creating flask app and choose folder for templates
+app.config.from_object(Config)
+db = SQLAlchemy(app) #creating database 
+migrate = Migrate(app, db) # migrate using for upgrade database, like as adding new tables.
+
+from app import routes, models
