22a651a026eb06b8bfd377b1751b69f73feb9fbd
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-12-06 20:45:34 +0200

    added new table Admin. The Admin table is used to store nicknames and hashed passwords

--- api/app/models.py
+++ api/app/models.py
@@ -10,6 +10,17 @@ class Posts(db.Model):
 class Threads(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     thread_id = db.Column(db.Integer, unique = True)
+    thread_name = db.Column(db.String(64))
     count_of_posts = db.Column(db.Integer) # counting post, using for thread_bamping
     theme = db.Column(db.String(2)) # where thread must be
     thread_bamping = db.Column(db.Integer)  # if thread has more than 100 posts it will not get to the top of pages
+
+class Themes(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    theme_name = db.Column(db.String(64), unique = True)
+    theme_context = db.Column(db.String(256))
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True)
+    password = db.Column(db.String(256))
