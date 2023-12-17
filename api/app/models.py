3818cceb27747764ef9ebab0b7a4dd588667d456
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-12-17 20:06:03 +0200

    deleted columns in threads

--- api/app/models.py
+++ api/app/models.py
@@ -5,14 +5,12 @@ class Posts(db.Model):
     thread_id = db.Column(db.Integer)
     post_text = db.Column(db.String(1024))
     post_date = db.Column(db.DateTime)
-
+    image_name = db.Column(db.String(64))
 class Threads(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    thread_id = db.Column(db.Integer, unique = True)
     thread_name = db.Column(db.String(64))
-    count_of_posts = db.Column(db.Integer) # counting post, using for thread_bamping
     theme = db.Column(db.String(2)) # where thread must be
-    thread_bamping = db.Column(db.Integer)  # if thread has more than 100 posts it will not get to the top of pages
+    thread_text = db.Column(db.String(2064))
 
 class Themes(db.Model):
     id = db.Column(db.Integer, primary_key = True)
