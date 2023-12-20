678ca17fb1a5a122247edb165f59386ccea681cc
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-12-20 20:22:47 +0200

    deleted hard coded path

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,10 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-UPLOAD_FOLDER = '/home/tema/NG-ach/api/uploads/images'
-#app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../../client/templates')) #creating flask app and choose folder for templates
-#app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'), static_folder='/../uploads', static_url_path='/../uploads')
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'),static_folder='/home/tema/NG-ach/api/uploads')
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__),'../uploads/images') #where images would save
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'),static_folder=os.path.join(os.path.dirname(__file__),'../uploads'))
 app.config.from_object(Config)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app) #creating database 
