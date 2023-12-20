496671dea0ac81d242cdef6f49675612fbb9d9a8
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-12-20 11:12:55 +0200

    new static folders

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-UPLOAD_FOLDER = '/home/tema/NG-ach/server_storage/images/'
+UPLOAD_FOLDER = '/home/tema/NG-ach/api/uploads/images'
 #app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../../client/templates')) #creating flask app and choose folder for templates
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../../client/templates'), static_folder='/home/tema/NG-ach/server_storage', static_url_path='/server_storage')
+#app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'), static_folder='/../uploads', static_url_path='/../uploads')
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'),static_folder='/home/tema/NG-ach/api/uploads')
 app.config.from_object(Config)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app) #creating database 
